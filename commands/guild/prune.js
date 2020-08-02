e321a7bbb063182c3a26f20fa25a7b0beb1570d3
BlueBerryFloof <66891806+BlueBerryFloof@users.noreply.github.com>
2020-08-02 16:27:18 -0700

    Updoot 1.8.6.1
    Added Github access

---
+++ commands/guild/prune.js
@@ -0,0 +1,43 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class PruneCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'prune',
+      aliases: ['delete-messages', 'bulk-delete'],
+      description: 'Delete up to 99 recent messages',
+      group: 'guild',
+      memberName: 'prune',
+      guildOnly: true,
+      userPermissions: ['MANAGE_CHANNELS', 'MANAGE_MESSAGES'],
+      clientPermissions: ['MANAGE_MESSAGES'],
+      args: [
+        {
+          key: 'deleteCount',
+          prompt: 'How many messages do you want to delete?',
+          type: 'integer',
+          validate: deleteCount => deleteCount < 100 && deleteCount > 0
+        }
+      ]
+    });
+  }
+
+  run(message, { deleteCount }) {
+    if (message.member.hasPermission('MANAGE_CHANNELS', 'MANAGE_MESSAGES')) {
+
+      message.channel
+        .bulkDelete(deleteCount)
+        .then(messages => message.say(`Deleted ${messages.size} messages`))
+        .catch(e => {
+          console.error(e);
+          return message.say(
+            'Something went wrong when trying to delete messages :('
+          );
+        });
+    } else {
+      return message.say(
+        'You do not have the permissions to do this action'
+      );
+    }
+  }
+};
