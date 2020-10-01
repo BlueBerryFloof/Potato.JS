2834639684d8095968e1380b3b8e982f04aece54
BlueBerry <66891806+BlueBerryFloof@users.noreply.github.com>
2020-09-30 20:12:37 -0700

    Update testedit.sh

--- testedit.sh
+++ testedit.sh
@@ -2,8 +2,8 @@ pause() {
     read -p "Press [Enter] key to continue..." fackEnterKey
 }
 while true; do
-	show_menu
-	read_options
+	show_menu2
+	read_options2
 done
 back() {
     return
@@ -12,29 +12,29 @@ rebuild() {
     wget -nv --show-progress https://raw.githubusercontent.com/BlueBerryFloof/Potato.JS/Installer/rebuild.sh
     sh rebuild.sh
     sleep 1
-    show_menu
-    read_options
+    show_menu2
+    read_options2
 }
 enable() {
     wget -nv --show-progress https://raw.githubusercontent.com/BlueBerryFloof/Potato.JS/Installer/enable.sh
     sh enable.sh
     sleep 1
-    show_menu
-    read_options
+    show_menu2
+    read_options2
 }
 disable() {
     wget -nv --show-progress https://raw.githubusercontent.com/BlueBerryFloof/Potato.JS/Installer/disable.sh
     sh disable.sh
     sleep 1
-    show_menu
-    read_options
+    show_menu2
+    read_options2
 }
 menukill() {
 	rm -f menu.sh
     rm -f testedit.sh
 	exit 0
 }
-show_menu() {
+show_menu2() {
     clear
     echo "~V${VER}~~~~~~~~~~~~~~~~~~~~~~~~"
     echo " P O T A T O . J S - C O N F I G"
@@ -45,7 +45,7 @@ show_menu() {
     echo "4. Back"
     echo "5. Exit"
 }
-read_options() {
+read_options2() {
     local choicetwo
     read -p "Enter choice [ 1 - 5 ] " choicetwo
     case $choicetwo in
