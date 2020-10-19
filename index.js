a1a5b4804e79f67616d4cde8e6a7e16c79570971
BlueBerry <66891806+BlueBerryFloof@users.noreply.github.com>
2020-10-19 15:45:15 -0700

    Added support for a support server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { CommandoClient } = require('discord.js-commando');
 const { Structures } = require('discord.js');
 const path = require('path');
-const { prefix, token, discord_owner_id } = require('./config.json');
+const { prefix, support, token, discord_owner_id } = require('./config.json');
 Structures.extend('Guild', function(Guild) {
   class MusicGuild extends Guild {
     constructor(client, data) {
@@ -26,7 +26,8 @@ Structures.extend('Guild', function(Guild) {
 
 const client = new CommandoClient({
   commandPrefix: prefix,
-  owner: discord_owner_id // value comes from config.json
+  owner: discord_owner_id, // value comes from config.json
+  invite: support
 });
 
 client.registry
